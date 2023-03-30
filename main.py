67c72c1525da56f671112948e675877b925ac810
barryjung <wjdgusrbs5@gmail.com>
2023-03-30 17:34:14 +0900

    add levelup function

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def sh_state_up(a):
 
 
 def sh_max_exp_up(a):
-    return int(a * (1.0 + 70.0 / 100.0))
+    return int(a * (1.0 + 180.0 / 100.0))
 
 
 class Object():
@@ -38,7 +38,7 @@ class Object():
         damage = first_damage - int(monster.defense * 0.4)
         monster.HP = max(monster.HP - damage, 0)
         print(f"{self.name}의 공격! {monster.name}에게 {damage}의 데미지를 입혔습니다.")
-        time.sleep(2)
+        # time.sleep(2)
         if monster.HP == 0:
             print(f"{monster.name}(이)가 쓰러졌습니다.")
             time.sleep(2)
@@ -93,7 +93,7 @@ class Character(Object):
         if self.MP == 0:
             print("보유 중인 MP가 모자랍니다! 스킬을 사용할 수 없습니다!")
             return
-        debuff = random.randint(self.defense, int(self.defense * 1.3))
+        debuff = random.randint(self.defense, int(self.defense * 1.5))
         monster.defense = max(monster.defense - debuff, 0)
         print(f"{self.name}의 {self.skill}! {monster.name}의 방어력을 {debuff}만큼 깎았습니다.")
         if monster.hp == 0:
@@ -142,13 +142,17 @@ class Character(Object):
 
     # exp의 값이 max_exp의 값과 같거나 넘칠 때 레벨업과 레벨업 print 출력, exp값 유지하는 대신 max_exp의 값만 비례값으로 올라갑니다.
     # 전투 종료시 경험치 드랍과 함께 매번 추가해주시는 함수와 같다고 생각해주시면 될 것 같습니다. return 선언 방식이 잘못되었다면 주석 달아주시거나 말 걸어주세요!!
-    def Level_up(self):
-        if self.exp >= self.max_exp:
-            self.level+1
-            print(f'{self.name}가 LV.{self.level}로 레벨업했습니다!')
-            sh_max_exp_up(self.max_exp)
-        else:
-            return
+    def level_up(self, _exp):
+        self.exp = _exp
+        while True:
+            if self.exp >= self.max_exp:
+                self.level += 1
+                print(f'{self.name}가 LV.{self.level}로 레벨업했습니다!')
+                print(self.exp, self.max_exp)
+                sh_max_exp_up(self.max_exp)
+            else:
+                break
+        return
 
     def level_plus1(self):
         self.max_HP = sh_state_up(self.max_HP)
@@ -172,14 +176,27 @@ class Monster(Object):
         self.defense = int(defense * (1 + weight))
         self.speed = int(speed * (1 + weight))
 
-    # 특수공격_냥검사 / 마나값 임의 / 공격값은 공격력비례 임의의 랜덤(기본 ~ +50%)값 - 몬스터의 방어력비례(30퍼) / 임의값입니다
 
-    def dealing_Skill(self, monster):
-        self.MP = max(self.MP - 30, 0)
-        if self.mp == 0:
-            print("보유 중인 MP가 모자랍니다! 스킬을 사용할 수 없습니다!")
-            return
-        first_damage = random.randint(self.attack, int(self.attack * 1.5))
+class Boss(Object):
+    def __init__(self, name, level, HP, MP, attack, defense, speed, skill) -> None:
+        super().__init__(name, level, HP, MP, attack, defense, speed, skill)
+
+    # 슈뢰딩거의상_자식_5턴 / 기절 턴 수는 랜덤값,
+    def boss_cc(self, monster):
+        healing = max(self.HP+int(self.HP*0.3), self.max_HP)
+        print(f"{self.name}의 {self.skill}! {self.name}의 HP를 {healing}만큼 회복했습니다.")
+        if monster.hp == 0:
+            print(f"{monster.name}(이)가 쓰러졌습니다.")
+            time.sleep(2)
+            return True
+        else:
+            return False
+
+    # 잼민스킬_돌던지기_5턴, 제리스킬_후라이팬 샷_3턴 / 잼민무리.. 중간보스지만 2명이면 좋을 것 같아요 인당 1캐릭씩 랜덤타깃으로 생각했습니다
+    # 공격값은 스피드비례 랜덤(기본 ~ +50%)값 - 캐릭터의 방어력비례(30%) / 임의값입니다
+    # Print문 검토 필요@@!!
+    def boss_attack(self, monster):
+        first_damage = random.randint(self.speed, int(self.speed * 1.5))
         damage = first_damage - int(monster.defense * 0.3)
         monster.HP = max(monster.HP - damage, 0)
         print(f"{self.name}의 {self.skill}! {monster.name}에게 {damage}의 데미지를 입혔습니다.")
@@ -189,6 +206,21 @@ class Monster(Object):
             return True
         else:
             return False
+
+    # 제리의볼링쇼/ 누적8턴째(후라이팬+5턴뒤) / 광역스킬 / 공격값은 공격력비례 랜덤(기본+20% ~ +60%) - 캐릭터방어력비례(30%) / 임의값입니다
+    def jerry_attack(self, monster):
+        first_damage = random.randint(
+            int(self.attack * 1.2), int(self.attack * 1.6))
+        damage = first_damage - int(monster.defense * 0.3)
+        monster.HP = max(monster.HP - damage, 0)
+        print(f"{self.name}의 {self.skill}! {monster.name}에게 {damage}의 데미지를 입혔습니다.")
+        if monster.hp == 0:
+            print(f"{monster.name}(이)가 쓰러졌습니다.")
+            time.sleep(2)
+            return True
+        else:
+            return False
+
 
 # 아이템 - 윤성훈
 
@@ -205,6 +237,7 @@ class Equipment_Item(Item):
         self.attack_effect = attack_effect
 
     def equip(self, player):
+        self.player = player
         player.attack += self.attack_effect
         print(f"{player.name}의 공격력이 {self.attack_effect}만큼 증가했습니다!")
 
@@ -218,7 +251,7 @@ class Potion_Item(Item):  # --> 여기에 개수 좀 넣어볼게용
 
     def take_potion(self, player):
         self.num -= 1
-
+        self.player = player
         player.HP += self.hp_effect
         print(f"{player.name}의 HP가 {self.hp_effect}만큼 회복됐습니다!")
         player.MP += self.mp_effect
@@ -239,10 +272,10 @@ class Pet(Item):
 # 장비 아이템이랑 물약 아이템이 겹치는 게 있으니 Item이라는 클래스 두고 Equipment_Item, Potion_Item가 상속하는 건 어때요
 ##
 characters = {
-    "1": Character("냥검사", 1, 150, 30, 15, 20, 13, "냥냥펀치"),
-    "2": Character("냥법사", 1, 120, 50, 12, 13, 10, "꾹꾹이"),
-    "3": Character("냥궁수", 1, 100, 40, 18, 10, 18, "하악질"),
-    "4": Character("냥힐러", 1, 140, 50, 9, 16, 14, "그루밍")
+    "1": Character("냥검사", 1, 1500, 30, 150, 20, 13, "냥냥펀치",),
+    "2": Character("냥법사", 1, 1200, 50, 120, 13, 10, "꾹꾹이"),
+    "3": Character("냥궁수", 1, 1000, 40, 180, 10, 18, "하악질"),
+    "4": Character("냥힐러", 1, 1400, 50, 90, 16, 14, "그루밍")
 }
 
 # player = Character("냥검사", 1, 150, 30, 15, 20, 13)
@@ -250,40 +283,41 @@ characters = {
 # player = Character("냥궁수", 1, 100, 40, 18, 10, 18)
 # player = Character("냥힐러", 1, 140, 50, 9, 16, 14)
 
-# 이렇게 해야되는게 맞나?
 
-# 장비 아이템 장착
-equipment_items = {
-    "1": Equipment_Item("날카로운 발톱", "공격력을 10만큼 올려줍니다.", 10),  # 냥검사 전용 장비
-    "2": Equipment_Item("방울", "공격력을 10만큼 올려줍니다.", 10),  # 냥법사 전용 장비
-    "3": Equipment_Item("새총", "공격력을 10만큼 올려줍니다.", 10),  # 냥궁수 전용 장비
-    "4": Equipment_Item("장난감 막대", "공격력을 10만큼 올려줍니다.", 10)  # 냥힐러 전용 장비
-}
-equipment_items["1"].equip(characters["1"])  # 냥검사에 날카로운 발톱 장착
-equipment_items["2"].equip(characters["2"])  # 냥법사에 날카로운 발톱 장착
-equipment_items["3"].equip(characters["3"])  # 냥궁수에 날카로운 발톱 장착
-equipment_items["4"].equip(characters["4"])  # 냥힐러에 날카로운 발톱 장착
+# ------------------
 
-# equipment = Equipment_Item("날카로운 발톱", "공격력을 10만큼 올려줍니다.", 10) #냥검사 전용 아이템
-# equipment = Equipment_Item("방울", "공격력을 10만큼 올려줍니다.", 10) #냥법사 전용 아이템
-# equipment = Equipment_Item("새총", "공격력을 10만큼 올려줍니다.", 10) #냥궁수 전용 아이템
-# equipment = Equipment_Item("장난감 막대", "공격력을 10만큼 올려줍니다.", 10) #냥힐러 전용 아이템
-# equipment.equip(player)
+# # 장비 아이템 장착
+# equipment_items = {
+#     "1": Equipment_Item("날카로운 발톱", "공격력을 10만큼 올려줍니다.", 10),  # 냥검사 전용 장비
+#     "2": Equipment_Item("방울", "공격력을 10만큼 올려줍니다.", 10),  # 냥법사 전용 장비
+#     "3": Equipment_Item("새총", "공격력을 10만큼 올려줍니다.", 10),  # 냥궁수 전용 장비
+#     "4": Equipment_Item("장난감 막대", "공격력을 10만큼 올려줍니다.", 10)  # 냥힐러 전용 장비
+# }
+# equipment_items["1"].equip(characters["1"])  # 냥검사에 날카로운 발톱 장착
+# equipment_items["2"].equip(characters["2"])  # 냥법사에 날카로운 발톱 장착
+# equipment_items["3"].equip(characters["3"])  # 냥궁수에 날카로운 발톱 장착
+# equipment_items["4"].equip(characters["4"])  # 냥힐러에 날카로운 발톱 장착
+
+equipment1 = Equipment_Item("날카로운 발톱", "공격력을 10만큼 올려줍니다.", 10)  # 냥검사 전용 아이템
+equipment2 = Equipment_Item("방울", "공격력을 10만큼 올려줍니다.", 10)  # 냥법사 전용 아이템
+equipment3 = Equipment_Item("새총", "공격력을 10만큼 올려줍니다.", 10)  # 냥궁수 전용 아이템
+equipment4 = Equipment_Item("장난감 막대", "공격력을 10만큼 올려줍니다.", 10)  # 냥힐러 전용 아이템
+
 
 # 물약 사용
 
-potion_items = {
-    "참치캔": Potion_Item("참치캔", "HP를 20만큼 채워줍니다.", 20, 0, 0),
-    "츄르": Potion_Item("츄르", "MP를 20만큼 채워줍니다.", 0, 20, 0),
-    "북어포": Potion_Item("북어포", "HP를 50만큼 채워줍니다.", 50, 0, 0),
-    "캣닢": Potion_Item("캣", "MP를 50만큼 채워줍니다.", 0, 50, 0)
-}
+# potion_items = {
+#     "참치캔": Potion_Item("참치캔", "HP를 20만큼 채워줍니다.", 20, 0, 0),
+#     "츄르": Potion_Item("츄르", "MP를 20만큼 채워줍니다.", 0, 20, 0),
+#     "북어포": Potion_Item("북어포", "HP를 50만큼 채워줍니다.", 50, 0, 0),
+#     "캣닢": Potion_Item("캣", "MP를 50만큼 채워줍니다.", 0, 50, 0)
+# }
 
-# potion = Potion_Item("참치캔", "HP를 20만큼 채워줍니다.", 20, 0)
-# potion = Potion_Item("츄르", "MP를 20만큼 채워줍니다.", 0, 20)
-# potion = Potion_Item("북어포", "HP를 50만큼 채워줍니다.", 50, 0)
-# potion = Potion_Item("털실뭉치", "MP를 50만큼 채워줍니다.", 0, 50)
-# potion.take_potion(player)
+potion1 = Potion_Item("참치캔", "HP를 20만큼 채워줍니다.", 20, 0, 0)
+potion2 = Potion_Item("츄르", "MP를 20만큼 채워줍니다.", 0, 20, 0)
+potion3 = Potion_Item("북어포", "HP를 50만큼 채워줍니다.", 50, 0, 0)
+potion4 = Potion_Item("캣닢", "MP를 50만큼 채워줍니다.", 0, 50, 0)
+
 
 pet1 = Pet('털뭉치', '방어력을 5만큼 증가시켜줍니다.', 5, 10)
 pet2 = Pet('pet2', '방어력을 10만큼 증가시켜줍니다.', 10, 15)
@@ -334,14 +368,14 @@ def prebattle():
 def battle(players, monsters):
     os.system(clear)
     print("길을 걷다 천적을 발견했다!")
-    time.sleep(2)
+    # time.sleep(2)
     print(*monsters, end=" ")
     print("이(가) 튀어나왔다!")
-    time.sleep(2)
+    # time.sleep(2)
     print("가라!", end=" ")
     print(*players, end=" ")
     print(" ")
-    time.sleep(2)
+    # time.sleep(2)
 
     # 턴 시작 판정
     player_avg_speed = 0
@@ -380,7 +414,7 @@ def battle(players, monsters):
 
                 print("")
                 action = input(
-                    f"{players[i]}가 대기중입니다. 어떻게 하시겠습니까? [ 1) 일반공격 2) 스킬:{players[ } 3) 포션 사용 4) 도망친다 ] : ")
+                    f"{players[i]}가 대기중입니다. 어떻게 하시겠습니까? [ 1) 일반공격 2) 스킬:{players[i].skill} 3) 포션 사용 4) 도망친다 ] : ")
                 if action.isdigit() == False:
                     print("정수를 입력해주세요.")
                     time.sleep(2)
@@ -395,29 +429,31 @@ def battle(players, monsters):
                         target = input()
                         if target.isdigit() == False:
                             print("정수를 입력해주세요.")
-                            time.sleep(2)
+                            # time.sleep(2)
                         elif bool(re.search(f"[1-{len(monsters)}]", target)) == False:
                             print("잘못 입력했습니다. 다시 시도하세요.")
-                            time.sleep(2)
+                            # time.sleep(2)
                         else:
-                            # monsters[int(target) - 1] = monsters[int(target) - 1]
-                            check_ = players[i].normal_attack(
-                                monsters[int(target) - 1])
+                            target_monster = monsters[int(target) - 1]
+                            check_ = players[i].normal_attack(target_monster)
                             if check_ == True:
-                                earned_exp += int(monsters[int(target) - 1].level * 2 * random.randint(
-                                    1, 2) + monsters[int(target) - 1].max_HP * 1.2 * random.randint(1, 2))
-                                monsters[int(target) - 1] = '기절'
-
+                                earned_exp += int(target_monster.level * 2 * random.randint(
+                                    1, 2) + target_monster.max_HP * 1.2 * random.randint(1, 2))
+                                target_monster = '기절'
                                 cache = []
                                 for i in monsters:
-                                    if i.HP == 0:
+                                    if i.HP != 0:
                                         cache.append(i)
                                 monsters = cache
 
                             if len(monsters) == 0:
                                 print("승리!")
                                 time.sleep(2)
-                                drop_item(earned_exp)
+                                # drop_item(earned_exp)
+                                players[1].level_up(earned_exp)
+                                players[2].level_up(earned_exp)
+                                players[3].level_up(earned_exp)
+                                town()
 
                     elif action == 2:
                         pass
@@ -622,26 +658,29 @@ def buy_item():
 
             else:
                 print('1, 2, 3, 4 중에 선택해주십시오.')
-                # 다시 바깥if문으로..
+                break
 
-        # elif choice == '2':
-        #     print('장비 구매를 원하시는군요!')
-        #     print('장비 구매의 가격은 $30 입니다.')
+        elif choice == '2':
+            print('장비 구매를 원하시는군요!')
+            print('장비 구매의 가격은 $30 입니다.')
 
-        #     choice_equipment = input('구매하시겠습니까? y/n로 선택해주세요!\n:')
+            choice_equipment = input('구매하시겠습니까? y/n로 선택해주세요!\n:')
 
-        #     if choice_equipment == 'y':
-        #         for i in range(len(player_character_list)):
-        #             if bool(player_character_list[i].ep) == False
-        #         player.attack += 10
-        #         print(f"{player_character_list[].name}의 공격력이 10만큼 증가했습니다.")
+            if choice_equipment == 'y':
+                for i in range(len(player_character_list)):
+                    if bool(player_character_list[i].ep) == False:
+                        print(
+                            f'{player_character_list[i].name}의 장비를 구매하실 수 있습니다.')
 
-        #     elif choice_equipment == 'n':
-        #         continue
+                # player.attack += 10
+                # print(f"{player_character_list[].name}의 공격력이 10만큼 증가했습니다.")
 
-        #     else:
-        #         print('y/n로 선택해주세요!')
-        #         # 다시 바깥if문으로..
+            elif choice_equipment == 'n':
+                continue
+
+            else:
+                print('y/n로 선택해주세요!')
+                break
 
         elif choice == '3':
             print("귀여운 pet 구매를 원하시는군요! 탁월한 선택이십니다.^^")
@@ -669,7 +708,7 @@ def buy_item():
 
             else:
                 print('1, 2, 3 중에 선택해주십시오.')
-                # 다시 바깥if문으로..
+                break
 
         else:
             print('1, 2, 3 중에 선택해주세요!!')
@@ -711,7 +750,6 @@ def buy_item():
 
 
 """메인 시작"""
-
 # Character List
 # character = {
 #     '1': Character("냥검사", 1, 150, 30, 15, 20, 13),
@@ -737,7 +775,7 @@ else:
 os.system(clear)
 print("==========================")
 time.sleep(1)
-print("====== 고양이 게임 ========")
+print("====== 고양이 게임 =======")
 time.sleep(1)
 print("==========================")
 time.sleep(1)
@@ -772,16 +810,16 @@ os.system(clear)
 print("당신은", end=" ")
 print(*player_character_list, end="")
 print("를 선택했습니다. 이제 모험을 떠나볼까요.")
-time.sleep(4)
+# time.sleep(4)
 
 
 # Story #1. 길 걷기
 os.system(clear)
 print("여행을 떠난 당신은 길을 걷다 한 마을에 도착했습니다.")
-time.sleep(2)
+# time.sleep(2)
 
 print("마을을 한번 둘러볼까요.")
-time.sleep(2)
+# time.sleep(2)
 
 # 어떤 행동 끝나면 마을
 run = 0
